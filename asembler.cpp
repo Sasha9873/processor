aa9a90bf8b0b004f5d1c92355928afc6c027a3eb
Sasha9873 <78866386+Sasha9873@users.noreply.github.com>
2022-02-02 16:15:37 +0300

    Add files via upload

--- asembler.cpp
+++ asembler.cpp
@@ -18,9 +18,9 @@ int main()
 {
     struct processor proc = {};
 
-    FILE* file_asm = fopen("asm.txt", "r");
-    FILE* code_txt = fopen("code.txt", "w+");
-    proc.code_bin = fopen("code.bin", "w+");
+    FILE* file_asm = fopen("asm.txt", "rb");
+    FILE* code_txt = fopen("code.txt", "wb+");
+    proc.code_bin = fopen("code.bin", "wb+");
     printf("%p\n", file_asm);
 
     errors_t error;
