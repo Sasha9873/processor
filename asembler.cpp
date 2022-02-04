f42d08bcc16093bb17572b539f569ff393558a0f
Sasha9873 <78866386+Sasha9873@users.noreply.github.com>
2022-02-04 16:58:54 +0300

    Add files via upload

--- asembler.cpp
+++ asembler.cpp
@@ -16,13 +16,16 @@ int main()
     printf("%p\n", proc.code);
     proc.code = asembler(file_asm, code_txt, &error, &proc);
 
+    printf("error = %d\n", error);
+
     //printf("\n \n \n%n", proc.code);
     printf("\n    %ld\n", proc.size_code);
 
     fclose(file_asm);
     fclose(code_txt);
     fclose(proc.code_bin);
-    //free(proc.code);
+    free(proc.code);
+    proc.code = NULL;
 
     return 0;
 }
@@ -67,120 +70,15 @@ int* asembler(FILE* file_asm, FILE* code_txt, errors_t* error, struct processor*
 
         char trash[10];
 
-        int k1 = sscanf(str, "%s %d %s", cmd, &value, trash);
-        int k2 = sscanf(str, "%s %s %s", cmd, reg, trash);
-        printf("k1 = %d k2 = %d %s %s %s\n", k1, k2, cmd, reg, trash);
+        int k_str_num = sscanf(str, "%s %d %s", cmd, &value, trash);
+        int k_strs = sscanf(str, "%s %s %s", cmd, reg, trash);
+        //printf("k1 = %d k2 = %d %s %s %s\n", k1, k2, cmd, reg, trash);
 
-
-        if(strcmp(cmd, comands_names[PUSH]) == 0){
-
-            if(k1 == 2){
-                code[proc->ip] = PUSH;
-                printf("%d ", code[proc->ip]);
-                proc->ip++;
-
-                code[proc->ip] = value;
-
-                printf("%d\n", code[proc->ip]);
-                fprintf(code_txt,"%04d   %02x %02x\t\t PUSH %d\n", proc->ip - 1, PUSH, value, value);
-            }
-            else{
-                printf("reg\n");
-                code[proc->ip] = RPUSH;
-                printf("%d ", code[proc->ip]);
-                proc->ip++;
-
-                if(strcmp(reg, "rax") == 0)
-                    code[proc->ip] = 1;
-                else if(strcmp(reg, "rbx") == 0)
-                    code[proc->ip] = 2;
-                else if(strcmp(reg, "rcx") == 0)
-                    code[proc->ip] = 3;
-                else if(strcmp(reg, "rdx") == 0)
-                    code[proc->ip] = 4;
-                else{
-                    *error = BAD_REG;
-                    printf("BAD_REG");
-                    return code;
-                }
-
-
-                printf("%d\n", code[proc->ip]);
-                fprintf(code_txt,"%04d   %02x %02x\t\t RPUSH %d\n", proc->ip - 1, RPUSH, code[proc->ip], code[proc->ip]);
-            }
-            proc->ip++;
-
+        #include "commands.h"
+        /*else*/{*error = BAD_COMMAND;
+            return code;
         }
-
-        else if(strcmp(cmd, comands_names[POP]) == 0){
-            proc->ip++;
-
-            if(k2 == 2){
-                if(strcmp(reg, "rax") == 0)
-                    code[proc->ip] = 1;
-                else if(strcmp(reg, "rbx") == 0)
-                    code[proc->ip] = 2;
-                else if(strcmp(reg, "rcx") == 0)
-                    code[proc->ip] = 3;
-                else if(strcmp(reg, "rdx") == 0)
-                    code[proc->ip] = 4;
-                else{
-                    *error = BAD_REG;
-                    return code;
-                    }
-                code[proc->ip - 1] = RPOP;
-
-                printf("reg %d\n", code[proc->ip]);
-                fprintf(code_txt,"%04d   %02x %02x\t\t RPOP %d\n", proc->ip - 1, RPOP, code[proc->ip], code[proc->ip]);
-
-                proc->ip++;
-            }
-            else{
-                code[proc->ip - 1] = POP;
-                fprintf(code_txt,"%04d   %02x\t\t\t POP\n", proc->ip - 1, POP);
-            }
-            
-
-        }
-
-        else if(strcmp(cmd, comands_names[MUL]) == 0){
-            code[proc->ip] = MUL;
-            fprintf(code_txt,"%04d   %02x\t\t\t MUL\n", proc->ip, MUL);
-
-            proc->ip++;
-        }
-        else if(strcmp(cmd, comands_names[SUB]) == 0){
-            code[proc->ip] = SUB;
-            fprintf(code_txt,"%04d   %02x\t\t\t SUB\n", proc->ip, SUB);
-
-            proc->ip++;
-        }
-        else if(strcmp(cmd, comands_names[ADD]) == 0){
-            code[proc->ip] = ADD;
-            fprintf(code_txt,"%04d   %02x\t\t\t ADD\n", proc->ip, ADD);
-
-            proc->ip++;
-        }
-        else if(strcmp(cmd, comands_names[OUT]) == 0){
-            code[proc->ip] = OUT;
-            fprintf(code_txt,"%04d   %02x\t\t\t OUT\n", proc->ip, OUT);
-
-            proc->ip++;
-        }
-        else if(strcmp(cmd, comands_names[HLT]) == 0){
-            code[proc->ip] = HLT;
-            fprintf(code_txt,"%04d   %02x\t\t\t HLT\n", proc->ip, HLT);
-
-            proc->ip++;
-        }
-        else if(strcmp(cmd, comands_names[IN]) == 0){
-            code[proc->ip] = IN;
-            fprintf(code_txt,"%04d   %02x\t\t\t IN\n", proc->ip, IN);
-
-            proc->ip++;
-        }
-
-    }
+    }    
 
     code[proc->ip] = '\0';
 
@@ -193,10 +91,63 @@ int* asembler(FILE* file_asm, FILE* code_txt, errors_t* error, struct processor*
 
     fwrite(code, sizeof(code[0]), proc->ip, proc->code_bin);
 
+    /*printf("\n \n \ncode:%n", proc->code);
+
     free(code);
-    code = NULL;/**/
+    code = NULL;*/
 
     return code;
+
+}
+
+/*int init_arg(int* code, int k_str_num, int k_strs, FILE* code_txt, errors_t* error, struct processor* proc, char* name, int num)
+{
+    int arg_is = what_arg(k_str_num, k_strs, reg);
+
+    if(arg_is == -1){
+        *error = BAD_ENTRY;
+        return code;
+    }
+    else if(arg_is == 0){
+        proc->ip++;
+        code[proc->ip] = value;
+        
+        printf("%d\n", code[proc->ip]);
+
+        fprintf(code_txt,"%04d   %02x %02x\t\t ", proc->ip - 1, num, value);
+        fprintf(code_txt,"%s %d\n", name, value);
+    }
+    else{
+        proc->ip++;
+
+        printf("%d ", code[proc->ip]);
+
+        code[proc->ip] = arg_is;
+            
+        printf("%d\n", code[proc->ip]);
+
+        fprintf(code_txt,"%04d   %02x ", proc->ip - 1, num);
+        fprintf(code_txt,"%02x\t\t %s ", code[proc->ip], name);
+        fprintf(code_txt,"%d\n", code[proc->ip]);
+
+    }
+}*/
+
+int what_arg(int k_str_num, int k_strs, char* reg)
+{
+    if(k_str_num == 2)
+        return 0;
+
+    if(strcmp(reg, "rax") == 0)
+        return 1;
+    else if(strcmp(reg, "rbx") == 0)
+        return 2;
+    else if(strcmp(reg, "rcx") == 0)
+        return 3;
+    else if(strcmp(reg, "rdx") == 0)
+        return 4;
+
+    return -1;
 
 }
 
--- asm.h
+++ asm.h
@@ -1,8 +1,9 @@
 typedef enum errors{
-    ALL_OK     =  0,
-    NOT_MEMORY = -1,
-    BAD_REG    = -2,
-    BAD_ENTRY  = -3,
+    ALL_OK      =  0,
+    NOT_MEMORY  = -1,
+    BAD_REG     = -2,
+    BAD_ENTRY   = -3,
+    BAD_COMMAND = -4,
 }errors_t;
 
 
@@ -14,96 +15,51 @@ int listing(FILE* file_lst, int* code);
  *         1, 2, 3, 4 if it is a register rax, rbx, rcx, rdx respectively 
  *        -1          if it is a fault.
  */
-int what_arg(int k_str_num, int k_strs);
+int what_arg(int k_str_num, int k_strs, char* reg);
 
 /**
  * 
  */
-int arg(int* code, int k_str_num, int k_strs, FILE* code_txt, errors_t* error, struct processor* proc, char* name, int num);
+int init_arg(int* code, int arg_is, FILE* code_txt, errors_t* error, struct processor* proc, char* name, int num);
 
 
 #define DEFCMD(name, num, arg, ...)\
-if (stricmp(cmd, #name) == 0){\
+if (strcmp(cmd, #name) == 0){\
     code[proc->ip] = num;\
     if(arg){\
-        if(arg(code, k1, k2, code_txt, error, proc, name, num)){\
-            *error = BAD_ENTRY;\
-            return code;\
-        }\
-
-        int arg_is = what_arg(k_str_num, k_strs);\
-
+        int arg_is = what_arg(k_str_num, k_strs, reg);\
+        \
         if(arg_is == -1){\
+            printf("arg_is = %d\n", arg_is);\
             *error = BAD_ENTRY;\
             return code;\
         }\
-        else if(arg_is == 0){
-            proc->ip++;
-            code[proc->ip] = value;
-            
-            printf("%d\n", code[proc->ip]);
-
-            fprintf(code_txt,"%04d   %02x %02x\t\t ", proc->ip - 1, num, value);
-            fprintf(code_txt,"%s %d\n", name, value);
-        }
-        else{
-            proc->ip++;
-
-            printf("%d ", code[proc->ip]);
-
-            code[proc->ip] = arg_is;
-            
-            printf("%d\n", code[proc->ip]);
-
-            fprintf(code_txt,"%04d   %02x ", proc->ip - 1, num);
-            fprintf(code_txt,"%02x\t\t %s ", code[proc->ip], name);
-            fprintf(code_txt,"%d\n", code[proc->ip]);
-
-        }
-
-    }
-    else if(k2 != 1){
-        *error = BAD_ENTRY;
-        return code;
-    }
-
-    if(strcmp(cmd, comands_names[PUSH]) == 0){
-
-            if(k1 == 2){
-                code[proc->ip] = PUSH;
-                printf("%d ", code[proc->ip]);
-                proc->ip++;
-
-                code[proc->ip] = value;
-
-                printf("%d\n", code[proc->ip]);
-                fprintf(code_txt,"%04d   %02x %02x\t\t PUSH %d\n", proc->ip - 1, PUSH, value, value);
-            }
-            else{
-                printf("reg\n");
-                code[proc->ip] = RPUSH;
-                printf("%d ", code[proc->ip]);
-                proc->ip++;
-
-                if(strcmp(reg, "rax") == 0)
-                    code[proc->ip] = 1;
-                else if(strcmp(reg, "rbx") == 0)
-                    code[proc->ip] = 2;
-                else if(strcmp(reg, "rcx") == 0)
-                    code[proc->ip] = 3;
-                else if(strcmp(reg, "rdx") == 0)
-                    code[proc->ip] = 4;
-                else{
-                    *error = BAD_REG;
-                    printf("BAD_REG");
-                    return code;
-                }
-
-
-                printf("%d\n", code[proc->ip]);
-                fprintf(code_txt,"%04d   %02x %02x\t\t RPUSH %d\n", proc->ip - 1, RPUSH, code[proc->ip], code[proc->ip]);
-            }
-            proc->ip++;
-
-        }
+        else if(arg_is == 0){\
+            proc->ip++;\
+            code[proc->ip] = value;\
+            printf("%d\n", code[proc->ip-1]);\
+            fprintf(code_txt,"%04d   %02x %02x\t ", proc->ip - 1, num, value);\
+            fprintf(code_txt,"%s %d\n", #name, value);\
+        }\
+        else{\
+            proc->ip++;\
+            printf("%d ", code[proc->ip-1]);\
+            code[proc->ip] = arg_is;\
+            printf("%d\n", code[proc->ip]);\
+            fprintf(code_txt,"%04d   %02x ", proc->ip - 1, num);\
+            fprintf(code_txt,"%02x\t %s ", code[proc->ip], #name);\
+            fprintf(code_txt,"%d\n", code[proc->ip]);\
+        }\
+    }\
+    else if(k_strs != 1){\
+        printf("k_strs = %d, name = %s\n", k_strs, #name);\
+        *error = BAD_ENTRY;\
+        return code;\
+    }\
+    else{\
+        fprintf(code_txt,"%04d   %02x\t\t %s\n", proc->ip, num, #name);\
+    }\
+    proc->ip++;\
+}\
+else\
 
